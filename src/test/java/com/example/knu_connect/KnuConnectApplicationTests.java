aa9df38b87ed3bf102b0c233cbed2169f2661c0e
seongwon02 <seongwon2303@gmail.com>
2025-09-28 18:22:54 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/example/knu_connect/KnuConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knu_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
