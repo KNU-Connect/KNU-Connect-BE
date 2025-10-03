83ee22555ebea73282db67c9ce3165f354802dfa
seongwon02 <seongwon2303@gmail.com>
2025-10-03 17:31:38 +0900

    feat(exception): 기본적인 예외 처리 로직 구현

---
+++ src/main/java/com/example/knu_connect/global/exception/common/BusinessException.java
@@ -0,0 +1,21 @@
+package com.example.knu_connect.global.exception.common;
+
+
+public class BusinessException extends RuntimeException {
+    
+    private final ErrorCode errorCode;
+    
+    public BusinessException(ErrorCode code) {
+        super(code.message);
+        this.errorCode = code;
+    }
+    
+    public BusinessException(ErrorCode code, String detail) {
+        super(detail != null ? detail : code.message);  // 상세 메시지로 덮어쓰기 가능
+        this.errorCode = code;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
