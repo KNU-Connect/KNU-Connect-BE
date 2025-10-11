140e9cbed7b435ee02221a5ca27ed77e926f785a
hyeon-seo <rrkdgustj727@gmail.com>
2025-10-11 20:22:34 +0900

    feat(auth): h2와 spring security 관련 보안 설정 추가

--- src/main/java/com/example/knu_connect/domain/auth/config/SecurityConfig.java
+++ src/main/java/com/example/knu_connect/domain/auth/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.knu_connect.domain.auth.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,16 +13,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private String[] allowdUrls = {"/h2-console/**"}; // 인증 없이 허용할 url 목록
+
     // 보안 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(form -> form.disable()) // 기본 로그인폼 비활성화
-                .httpBasic(basic -> basic.disable()) // HTTP Basic 인증 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인폼 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll() // 모든 요청 허용 (테스트용)
                         .anyRequest().authenticated()
-                );
+                )
+
+                // h2와 spring security를 같이 쓰기 위한 설정
+                // .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")) // h2 콘솔은 csrf 체크 제외
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // h2 콘솔은 iframe 허용
 
         return http.build();
     }
