a7ff0a03d7f2a582fc9455df45c76fb13ccddf7e
hyeon-seo <rrkdgustj727@gmail.com>
2025-10-26 19:58:30 +0900

    feat(auth):로그인 후 요청에 대한 검증 필터 구현

--- src/main/java/com/example/knu_connect/domain/auth/config/SecurityConfig.java
+++ src/main/java/com/example/knu_connect/domain/auth/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.knu_connect.domain.auth.config;
 
+import com.example.knu_connect.domain.auth.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,12 +13,14 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final String[] allowedUrls = {"/h2-console/**", "/swagger-ui/**", "/api-docs/**", "/actuator/health",
             "/api/auth/signup", "/api/auth/login", "/api/auth/email/**", "api/auth/refresh"}; // 인증 없이 허용할 url 목록
 
@@ -26,15 +28,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
+                .csrf(AbstractHttpConfigurer::disable)      // csrf 비활성화
                 .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인폼 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(allowedUrls).permitAll()
                         .anyRequest().authenticated()
                 )
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // h2 콘솔은 iframe 허용
-
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // h2 콘솔은 iframe 허용
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
