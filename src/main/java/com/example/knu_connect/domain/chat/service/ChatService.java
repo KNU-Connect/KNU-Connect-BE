b06121a625e0193cb3f8dfea9a082397f5e99d5c
seongwon02 <seongwon2303@gmail.com>
2025-11-09 17:18:47 +0900

    feat(chat): 채팅 서비스 구조

---
+++ src/main/java/com/example/knu_connect/domain/chat/service/ChatService.java
@@ -0,0 +1,26 @@
+package com.example.knu_connect.domain.chat.service;
+
+import com.example.knu_connect.domain.chat.dto.request.ChatMessageSendRequestDto;
+import com.example.knu_connect.domain.chat.dto.request.ChatRoomCreateRequestDto;
+import com.example.knu_connect.domain.chat.dto.response.*;
+
+public interface ChatService {
+
+    ChatRoomCreateResponseDto createChatRoom(Long userId, ChatRoomCreateRequestDto request);
+
+    ChatMessageResponseDto sendMessage(Long userId, Long chatRoomId, ChatMessageSendRequestDto request);
+
+    ChatMessageListResponseDto getChatMessageList(Long userId, Long chatRoomId, Long cursor, int size);
+
+    ChatRoomListResponseDto getChatRoomList(Long userId);
+
+    void deleteMessage(Long userId, Long chatRoomId, Long chatId);
+
+    void leaveChatRoom(Long userId, Long chatRoomId);
+
+    void openChatRoom(Long userId, Long chatRoomId);
+
+    void closeChatRoom(Long userId, Long chatRoomId);
+
+    void refreshChatRoom(Long userId, Long chatRoomId);
+}
